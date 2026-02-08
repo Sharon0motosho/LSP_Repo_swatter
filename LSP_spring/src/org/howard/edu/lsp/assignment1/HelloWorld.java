c25504ae1991d92e3ebca762471f80ff48cb1bc9
Sharon0motosho <kanyinsharon@gmail.com>
2026-02-08 15:09:24 -0500

    Merge branch 'main' of https://github.com/Sharon0motosho/LSP_Repo_swatter into main

---
+++ LSP_spring/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+
+public class HelloWorld {
+	
+	/**
+	 * Name: Sharon Omotosho
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
